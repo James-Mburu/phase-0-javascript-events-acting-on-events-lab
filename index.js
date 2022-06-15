6a7544c26f574cead23da54eabec00029526f48b
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-15 13:50:10 +0300

    commit tree 28c2bafc2683823f931dfb1f4a456113fec00e2e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4"
+//     function moveDodgerLeft(){
+//         dodger.style.left = "100px"
+// }
+
+
+//     function moveDodgerRight(){
+//         var left = dodger.style.left
+//         left = parseInt(left)
+//         left = parseInt(left)
+
+//         var newPosition = dodger.style.right
+//         newPosition = parseInt(newPosition)
+    
+//     }
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight(){
+    var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left >100) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
